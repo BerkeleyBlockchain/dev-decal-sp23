40b49f3ea07a3b73b49e3637ebfa6adf2a7ccfe2
Siddhant Satapathy <siddhant.satapathy1@gmail.com>
2023-02-23 16:13:17 -0800

    Added HW3 code

---
+++ hw3-Implementing NFT Marketplace/instructions.md
@@ -0,0 +1,16 @@
+# Building a project with Forge
+
+forge install OpenZeppelin/openzeppelin-contracts
+
+ - create git repo before installing this
+ - might need to change solidity settings in vscode extension
+
+ forge remappings > remappings.txt
+
+ Install solidity locally
+
+ Anvil
+
+ anvil
+
+ solidity script and deploy on anvil
---
+++ hw3-Implementing NFT Marketplace/test/NFTMarketplace.t.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/NFT.sol";
+import "../src/NFTMarketplace.sol";
+
+    
+
+contract NFTMarketplaceTest is Test {
+    NFTMarketplace public marketplace;
+    uint256 gasCost = 1e8;
+
+    
+    receive() external payable {}
+
+    function setUp() public {
+        marketplace = new NFTMarketplace("MarketplaceToken", "MKT", "Token");
+        vm.deal(address(1), 4 ether);
+        
+    }
+
+    function testSingleMintAndUnsold() public {
+        uint256 id1 = marketplace.createToken{value: marketplace.getListingPrice()}("Token 1", 2 ether);
+        assertEq(id1, 1);
+        NFTMarketplace.MarketItem[] memory unsold = marketplace.fetchMarketItems();
+        assertEq(unsold.length, 1);
+    }
+
+    function testListingPrice() public {
+        uint256 id1 = marketplace.createToken{value: 0.025 ether}("Token 1", 2 ether);
+        assertEq(id1, 1);
+
+        // this should go through
+        marketplace.updateListingPrice(0.030 ether);
+        emit log_named_uint("Listing price is now", marketplace.getListingPrice());
+        
+        // this should revert
+        vm.expectRevert();
+        vm.prank(address(1));
+        marketplace.updateListingPrice(0.030 ether);
+
+    }
+
+
+    function testMintAndBuy() public {
+        uint256 id1 = marketplace.createToken{value: 0.025 ether}("Token 1", 2 ether);
+        assertEq(id1, 1);
+        vm.startPrank(address(1));
+        marketplace.createMarketSale{value: 2 ether}(1);
+        assertEq(marketplace.fetchMyNFTs().length, 1);
+        assertEq(marketplace.fetchMarketItems().length, 0);
+        vm.stopPrank();
+    }
+
+    function testItemsListed() public {
+        uint256 id1 = marketplace.createToken{value: 0.025 ether}("Token 1", 2 ether);
+        assertEq(id1, 1);
+        assertEq(marketplace.fetchItemsListed().length, 1);
+        vm.startPrank(address(1));
+        marketplace.createMarketSale{value: 2 ether}(1);
+        assertEq(marketplace.fetchMarketItems().length, 0);
+        vm.stopPrank();
+        assertEq(marketplace.fetchItemsListed().length, 0);
+    }
+}
